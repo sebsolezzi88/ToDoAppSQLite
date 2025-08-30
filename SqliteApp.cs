8a293555fdea668d829e695df29da35d6390a4cb
seba <ssolezzi@gmail.com>
2025-08-30 15:32:31 -0300

    Funcion AddTask para agregar tareas

--- SqliteApp.cs
+++ SqliteApp.cs
@@ -6,7 +6,7 @@ namespace ToDoAppProgram
     {
         static readonly string connectionString = "Data Source=miBase.db";
         private static string sql;
-        
+
         /* Crea la tabla si no existe */
         public static void Init()
         {
@@ -23,6 +23,22 @@ namespace ToDoAppProgram
                 Console.WriteLine("Error al crear la base de base de detaos");
                 throw;
             }
-        } 
+        }
+        /* Agregar una tarea */
+        public static bool AddTask(string task)
+        {
+            using SqliteConnection conn = new(connectionString);
+            sql = "INSERT INTO tasks (task) VALUES (@task)";
+            using SqliteCommand cmd = new(sql, conn);
+            var rows = cmd.ExecuteNonQuery();
+            if (rows > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
